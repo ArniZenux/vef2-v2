302d2699393de9c7a8cc014b8c0c9b7fb9a81cdc
Arni Ingi Johannesson <arni.ingi@skaginn3x.com>
2022-02-10 20:45:37 +0000

    fix and change

--- src/routes/index-routes.js
+++ src/routes/index-routes.js
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import { body, validationResult } from 'express-validator';
+
 import { catchErrors } from '../lib/catch-errors.js';
 
 import { list, insert, update } from '../lib/db_psql.js';
+
+//import {userCheck, vidburdCheck } from '..lib/check.js';
 
 export const indexRouter = express.Router();
 
@@ -17,7 +21,6 @@ async function indexRoute(req, res) {
 }
 
 async function indexSlug(req, res){
-  console.log("hello arni");
   const id = [req.params.id];
   console.log(id); 
 
@@ -38,27 +41,15 @@ async function indexSlug(req, res){
   }
 }
 
-async function adminRoute(req, res){
-  console.log("hvar ertu ? ");
-  const errors = [];
-
-  const events = []; 
-
-  const formData = [];
-
-  res.render('admin', {errors, title: 'Viðburðasíðan - Umsjón', events, formData });
-}
-
 async function adminLogin(req, res){
   const message = [];
-  console.log("login aðgang");
+  
+  const errors = []; 
 
-  res.render('login', {message, title: 'Innskráningu stjóranda - Umsjón'});
+  res.render('login', {errors, message, title: 'Innskráningu stjóranda - Umsjón'});
 } 
 
 async function adminSlug(req, res){
-  console.log("update");
-
   const sluq = [req.params.slug];
   console.log(sluq);
 
@@ -97,11 +88,6 @@ async function indexSlugPost(req, res){
 }
 
 async function adminPost(req, res){
-  console.log("balbalablab");
-    
-  const user = [req.body.username];
-  const pass = [req.body.password];
-
   const sql_vidburdur = 'SELECT * FROM vidburdur';
   
   const rows = await list(sql_vidburdur);
@@ -116,7 +102,7 @@ async function adminPost(req, res){
 async function adminVidburdur(req, res){
   console.log("skrá nýja viðburði"); 
   
-  const info = [req.body.name, 'df-df', req.body.comment];
+  const info = [req.body.name, req.body.name, req.body.comment];
   
   let success = true;   
   
@@ -164,6 +150,130 @@ async function adminSlugPost(req, res){
   }
 }
 
+const nationalIdPattern = '^[0-9]{6}-?[0-9]{4}$';
+
+const userMiddleware = [
+  body('name')
+    .isLength({ min: 1 })
+    .withMessage('Nafn má ekki vera tómt'),
+  body('name')
+    .isLength({ max: 64 })
+    .withMessage('Nafn má að hámarki vera 64 stafir'),
+  body('comment')
+    .isLength({ min: 1 })
+    .withMessage('Vantar athugasemd'),
+  body('comment')
+    .isLength({ max: 400 })
+    .withMessage('Athugasemd má að hámarki vera 400 stafir'),
+];
+
+const adgangMiddleware = [
+  body('username')
+    .isLength({ min: 1 })
+    .withMessage('Notandi má ekki vera tómt'),
+  body('username')
+    .isLength({ max: 64 })
+    .withMessage('Notandi má að hámarki vera 64 stafir'),
+  body('password')
+    .isLength({ min: 1 })
+    .withMessage('Vantar lyklaorð'),
+  body('password')
+    .isLength({ max: 256 })
+    .withMessage('Hámark 256 stafir'),
+];
+
+const vidburdMiddleware = [
+  body('name')
+    .isLength({ min: 1 })
+    .withMessage('Nafn má ekki vera tómt'),
+  body('name')
+    .isLength({ max: 64 })
+    .withMessage('Nafn má að hámarki vera 64 stafir'),
+  body('comment')
+    .isLength({ min: 1 })
+    .withMessage('Vantar lýsing'),
+  body('comment')
+    .isLength({ max: 400 })
+    .withMessage('Hámarki vera 400 stafir'),
+];
+
+async function vidburdCheck(req, res, next) {
+  const { name, comment } = req.body;
+
+  const formData = {name, comment};
+
+  const validation = validationResult(req);
+
+  const sql_vidburdur = 'SELECT * FROM vidburdur';
+  const rows = await list(sql_vidburdur);
+
+  if (!validation.isEmpty()) {
+    return  res.render('admin', {errors : validation.errors, title: 'Viðburðasíðan - Umsjón', formData, events : rows });
+  }
+
+  return next();
+}
+
+async function adgangCheck(req, res, next) {
+  const { username, password } = req.body;
+
+  const formData = {username, password};
+
+  const validation = validationResult(req);
+
+  const message = [];
+
+  if (!validation.isEmpty()) {
+    return  res.render('login', {errors : validation.errors, message, title: 'Innskráningu stjóranda - Umsjón', formData});
+  }
+
+  return next();
+}
+
+async function userCheck(req, res, next) {
+  const { name, comment } = req.body;
+
+  const formData = {name, comment};
+
+  const validation = validationResult(req);
+
+  const id = [req.body.id];
+  console.log(id); 
+
+  const sql = 'SELECT * FROM vidburdur WHERE vidburdur.id = $1';
+  const sql_user = 'SELECT * FROM vidburdur, skraning WHERE vidburdur.id=skraning.event AND vidburdur.id = $1';
+
+  const rows = await list(sql, id); 
+  const rows_user = await list(sql_user, id); 
+    
+  if (!validation.isEmpty()) {
+    return res.render('vidburd', {errors: validation.errors, title: 'Viðburðasíðan', events : rows, users : rows_user, formData });
+  }
+  
+  return next();
+}
+
+async function updateCheck(req, res, next) {
+  const { name, slug, comment } = req.body;
+  
+  const slg = [req.body.slug];
+
+  const formData = {name, slug, comment};
+
+  const validation = validationResult(req);
+
+  console.log(slug); 
+
+  const sql = 'SELECT * FROM vidburdur WHERE vidburdur.slug = $1';
+
+  const rows = await list(sql, slg); 
+    
+  if (!validation.isEmpty()) {
+    return res.render('update', {errors: validation.errors, title: 'Uppfæra viðburð', events : rows, formData});
+  }
+
+  return next();
+}
 
 /**********/
 //   GET   /
@@ -172,11 +282,15 @@ indexRouter.get('/', catchErrors(indexRoute));
 indexRouter.get('/:id', catchErrors(indexSlug));
 indexRouter.get('/admin/login', catchErrors(adminLogin));
 indexRouter.get('/admin/:slug', catchErrors(adminSlug));
+indexRouter.get('/logout', (req, res) => {
+  req.logout();
+  res.redirect('/');
+});
 
 /**********/
 //  POST   /
 /**********/
-indexRouter.post('/admin', catchErrors(adminVidburdur));
-indexRouter.post('/admin/login', catchErrors(adminPost));
-indexRouter.post('/admin/:slug', catchErrors(adminSlugPost));
-indexRouter.post('/:slug', catchErrors(indexSlugPost));
+indexRouter.post('/admin', vidburdMiddleware, catchErrors(vidburdCheck), catchErrors(adminVidburdur));
+indexRouter.post('/admin/login', adgangMiddleware, catchErrors(adgangCheck), catchErrors(adminPost));
+indexRouter.post('/admin/:slug', vidburdMiddleware, catchErrors(updateCheck), catchErrors(adminSlugPost));
+indexRouter.post('/:slug', userMiddleware, catchErrors(userCheck),catchErrors(indexSlugPost));
